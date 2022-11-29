f415d4dcc1f0bac78f0d188450ada90fc4445f28
davelin18yufan <davelin30630@gmail.com>
2022-11-29 15:13:21 +0800

    server activated

---
+++ app.js
@@ -0,0 +1,26 @@
+//include package we need
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const restaurantList = require("./restaurant.json")
+
+//define variables
+const port = 3000;
+const app = express();
+
+//configure template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//static file
+app.use(express.static("public"));
+
+//root route
+app.get("/", (req, res) => {
+  res.send("server activated succeed")
+})
+
+//activate and listen
+app.listen(port, () => {
+  console.log("OK")
+})
